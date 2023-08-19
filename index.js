8e1e7729944db1b5be3fadc8e43e468077da66a1
Anandarupa Neogi <142035225+Anandarupa-17@users.noreply.github.com>
2023-08-19 18:13:12 +0530

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var num1 = Math.random() * 6;
 num1 = Math.floor(num1)+1;
-var randomImage1 = "./dice" + num1 + ".png";
-document.querySelector(".img1").setAttribute("src", randomImage1);
 var num2 = Math.random() * 6;
 num2 = Math.floor(num2)+1;
+var randomImage1 = "./dice" + num1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImage1);
 var randomImage2 = "./dice" + num2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomImage2);
 if (num1 > num2){
