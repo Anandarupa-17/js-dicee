949c80a7878b90e3f091b8ff7ad6940bffa46c17
Anandarupa Neogi <142035225+Anandarupa-17@users.noreply.github.com>
2023-08-19 18:09:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 = Math.random() * 6;
+num1 = Math.floor(num1)+1;
+var randomImage1 = "./images/dice" + num1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImage1);
+var num2 = Math.random() * 6;
+num2 = Math.floor(num2)+1;
+var randomImage2 = "./images/dice" + num2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImage2);
+if (num1 > num2){
+    document.querySelector("h1").textContent= "🚩Player 1 Wins!";
+}
+else if (num1 == num2){
+    document.querySelector("h1").textContent= "Draw!";
+}
+else{
+    document.querySelector("h1").textContent= "Player 2 Wins!🚩";
+}
